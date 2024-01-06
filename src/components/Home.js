e7b49214273d0f4ddd3cfa3cf31f858ed0bcbc41
ishita232 <127683263+Ishitabhatt23@users.noreply.github.com>
2024-01-05 19:24:15 -0500

    Add Skills.scss

---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+import myPDF from '../assets/Ishitabhatt-resume.pdf';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <section id="home" className="home">
+      <div className='home-main-contents'>
+        <h1>
+          Hi,{'\u00a0'}
+          
+          <span className='static-text'>I'm</span>{' '}
+          <span className='home_name'>Ishita</span>
+        </h1>
+
+        <div className='animation'>
+          <span className='static-text'>I'm{'\u00a0'}</span>
+          <div className="typewriter-container">
+            <Typewriter
+              options={{
+                autoStart: true,
+                loop: true,
+                delay: 40,
+                strings: [" a student", "an aspiring developer", "a coder"],
+              }}
+              className="typewriter-text"
+            />
+          </div>
+        </div>
+
+        <div className='home_resume'>
+          <a href={myPDF} download="Ishita Bhatt_Resume.pdf">
+            Download Resume
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
