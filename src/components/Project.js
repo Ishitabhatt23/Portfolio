ddaa055fb477ac155b58af0ce3aa3c967a663693
ishita232 <127683263+Ishitabhatt23@users.noreply.github.com>
2024-01-05 19:44:26 -0500

    Deployed website

---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Project.scss'
+export const Project = () => {
+  return (
+    <div>
+    <div className='Header1'>
+      <h2>Projects</h2>
+      </div>
+      </div>
+  )
+}
+export default Project;
